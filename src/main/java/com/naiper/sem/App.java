7097759c5361b80bd717438171fc708f14c10d79
PaulMuThaw <161428192+PaulMuThaw@users.noreply.github.com>
2026-09-22 13:45:25 +0630

    18th commit, lab3a

--- src/main/java/com/naiper/sem/App.java
+++ src/main/java/com/naiper/sem/App.java
@@ -1,38 +1,120 @@
 package com.naiper.sem;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
-import org.bson.Document;
+import java.sql.*;
 
-public class App
-{
-    public static void main(String[] args)
-    {
-        // Connect using MongoClients factory method with a valid connection URI
-        try (MongoClient mongoClient = MongoClients.create("mongodb://mongo-dbserver:27017")) {
+public class App {
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
 
-            // Get database
-            MongoDatabase database = mongoClient.getDatabase("mydb");
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect() {
+        try {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
 
-            // Get collection
-            MongoCollection<Document> collection = database.getCollection("test");
-
-            // Create document
-            Document doc = new Document("name", "Saw Paul Mu Thaw")
-                    .append("class", "DevOps")
-                    .append("year", "2024")
-                    .append("result", new Document("CW", 95).append("EX", 85));
-
-            // Add document
-            collection.insertOne(doc);
-
-            // Check document
-            Document myDoc = collection.find().first();
-            if (myDoc != null) {
-                System.out.println(myDoc.toJson());
+        int retries = 10;
+        for (int i = 0; i < retries; ++i) {
+            System.out.println("Connecting to database...");
+            try {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection(
+                        "jdbc:mysql://db:3306/employees?allowPublicKeyRetrieval=true&useSSL=false",
+                        "root",
+                        "example"
+                );
+                System.out.println("Successfully connected");
+                break;
+            } catch (SQLException sqle) {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            } catch (InterruptedException ie) {
+                System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
+
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect() {
+        if (con != null) {
+            try {
+                // Close connection
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+
+    /**
+     * Get employee details from ID.
+     */
+    public Employee getEmployee(int ID) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect = "SELECT emp_no, first_name, last_name FROM employees WHERE emp_no = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Return new employee if valid
+            if (rset.next()) {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+
+    /**
+     * Display employee details in console.
+     */
+    public void displayEmployee(Employee emp) {
+        if (emp != null) {
+            System.out.println(
+                    emp.emp_no + " " + emp.first_name + " " + emp.last_name + "\n" +
+                            emp.title + "\n" +
+                            "Salary:" + emp.salary + "\n" +
+                            emp.dept_name + "\n" +
+                            "Manager: " + emp.manager + "\n"
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+
+        // Display results
+        a.displayEmployee(emp);
+
+        // Disconnect from database
+        a.disconnect();
     }
 }
